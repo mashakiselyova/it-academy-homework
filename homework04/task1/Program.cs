c9af1f60fc77993a850d160f792e349a7b57d5be
Maria Kiselyova <mariya.kiselyova95@gmail.com>
2019-03-13 13:50:40 +0300

    Two console applications

---
+++ homework04/task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'Z'; i >= 'A'; i--)
+            {
+                Console.Write(i);
+            }
+            for (char i = 'z'; i >= 'a'; i--)
+            {
+                Console.Write(i);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ homework04/task2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                          
+                string input = Console.ReadLine();
+                if (input == "")
+                    break;
+                else
+                {
+                    char letter = Convert.ToChar(input);
+                    switch (letter)
+                    {
+                        case 'W':
+                        case 'w':
+                            Console.WriteLine("Move up");
+                            break;
+                        case 'S':
+                        case 's':
+                            Console.WriteLine("Move down");
+                            break;
+                        case 'A':
+                        case 'a':
+                            Console.WriteLine("Move left");
+                            break;
+                        case 'D':
+                        case 'd':
+                            Console.WriteLine("Move right");
+                            break;
+                        default:
+                            Console.WriteLine("Nowhere to move");
+                            break;
+                    }
+                
+                }
+            }
+        }
+    }
+}
