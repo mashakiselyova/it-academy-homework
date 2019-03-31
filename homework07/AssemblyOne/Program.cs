06651d229311568c580a37e2bd2e169d70c0d50a
Maria Kiselyova <mariya.kiselyova95@gmail.com>
2019-03-31 20:48:39 +0300

    Homework 7. Initial commit.

---
+++ homework07/AssemblyOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Public member in the same assembly: " + Employee.CompanyNamePublic);
+            Console.Write("Protected member from base class in the same assembly: ");
+            Employee.PrintProtectedMember();
+            Console.Write("Protected member from derived class in the same : ");
+            Developer.PrintProtectedMember();
+            Console.WriteLine("Internal member from base class: " + Employee.CompanyNameInternal);
+            Console.WriteLine("Internal member from derived class: " + Developer.CompanyNameInternal);
+            Console.WriteLine("Protected Internal member from base class in the same assembly: " + Employee.CompanyNameProtectedInternal);
+            Console.WriteLine("Protected Internal member from derived class in the same assembly: " + Developer.CompanyNameProtectedInternal);
+            Console.Write("Private member: ");
+            Employee.PrintPrivateMember();
+            Console.Write("Private Protected member from base class: ");
+            Employee.PrintPrivateProtected();
+            Console.Write("Private Protected member from derived class: ");
+            Developer.PrintPrivateProtected();
+
+            Console.Read();
+        }
+    }
+}
